3b3da3892deaf68aa81a001c0ad0d09948546d1f
Doug <git@pixlwave.uk>
2020-03-28 13:13:16 +0000

    Add Package.swift.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "F53OSC",
+    products: [
+        .library(
+            name: "F53OSC",
+            targets: ["F53OSC"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "F53OSC",
+            dependencies: [],
+            path: "F53OSC/Sources/F53OSC",
+            publicHeadersPath: "")
+    ]
+)
