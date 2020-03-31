c313d17e9c6f6b806ce755e12e728682957f92c8
Doug <git@pixlwave.uk>
2020-03-31 19:52:17 +0100

    Remove custom path from target. Add CocoaAsyncSocket as a dependency.

--- Package.swift
+++ Package.swift
@@ -9,12 +9,13 @@ let package = Package(
             name: "F53OSC",
             targets: ["F53OSC"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4")
+    ],
     targets: [
         .target(
             name: "F53OSC",
-            dependencies: [],
-            path: "F53OSC/Sources/F53OSC",
+            dependencies: ["CocoaAsyncSocket"],
             publicHeadersPath: "")
     ]
 )
